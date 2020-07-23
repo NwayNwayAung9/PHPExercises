e2cea8ae53939c8a0b2f301408c83be20ce76c23
NwayNwayAung9 <nwaynadiaung1@gmail.com>
2020-07-23 12:12:25 +0630

    hello

---
+++ tableColorTest.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<h4>Chess Table Color</h4>
+<table  border="1" cellpadding="3" cellspacing="0">
+<?php
+
+for($i=0;$i<8;$i++)
+{
+    echo "<tr >";
+   
+    for($j=0;$j<8;$j++)
+{
+    $r=$i+$j;
+   if($r%2==0 )
+   echo "<td  BGCOLOR=#000000 width=50; height=50;></td>";
+   else 
+   echo "<td  BGCOLOR=#ffffff  width=50; height=50;></td>";
+    
+}
+echo "</tr>";
+}
+?>
+</table>
+</body>
+</html>
+
+
+
